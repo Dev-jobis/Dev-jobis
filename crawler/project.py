2f5ab0a2664c100e6bd1dc711be4aa1634262e25
sang596332 <sang596332@naver.com>
2023-12-21 19:22:13 +0900

    크롤링한 파일 kafka로 넘어가는거 확인

--- crawler/project.py
+++ crawler/project.py
@@ -10,22 +10,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import TimeoutException
+from kafka import KafkaProducer
 import re
+import json
 
 
-# 크롬 옵션 설정 - detach:크롬이 닫혀도 웹 브라우저 유지 기능
 chrome_options = Options()
 chrome_options.add_argument('--headless')
-    # 브라우저 안보이고 사용 가능(테스트할 땐 끄고 올릴때 다시 켜기)
+   
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
 chrome_options.add_argument('--log-level=3')
 
 service = Service(executable_path=ChromeDriverManager().install())
-    #크롬 드라이버 설치 , 드라이버 설치 경로
+   
 driver = webdriver.Chrome(service=service, options=chrome_options)
-    # 최신 버전 크롬 드라이버 자동 다운로드, 사용
+  
 driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
     "source": """
             Object.defineProperty(navigator, 'webdriver', {
@@ -37,12 +38,11 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
 URL_LIST = []
 
 def MAKE_URL():
-    for i in range(195000, 195100, 1): # 리눅스로 크론탭할 때 변수 변경이 가능한가
+    for i in range(194000, 195100, 1): 
         URL = "https://www.wanted.co.kr/wd/" + str(i)
         URL_LIST.append(URL)
 
 MAKE_URL()
-# print(URL_LIST) - url리스트 잘 나오는지 확인
 
 completed_count = 0
 max_completed_count = len(URL_LIST)
@@ -51,8 +51,9 @@ while completed_count < max_completed_count:
 
     for i, URL in enumerate(URL_LIST):
         response = requests.get(URL)
-        if response.status_code == 200:   
-
+        if response.status_code == 200:
+            
+            
             driver.get(URL)
             try:
                 WebDriverWait(driver, 20).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
@@ -62,8 +63,7 @@ while completed_count < max_completed_count:
             page_source = driver.page_source
             soup = bs(page_source, 'html.parser')
             title = soup.title.text
-            # print(title)
-            # 채용공고 이름 확인용 print
+           
             details = soup.select("#__next > div.JobDetail_cn__WezJh > \
                                     div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > \
                                     div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > \
@@ -77,7 +77,7 @@ while completed_count < max_completed_count:
                     jikmoo = soup[soup.find('"occupationalCategory":') + 24: soup.find('"employmentType"') - 2]
             elif soup.find('"sub_categories":')!= -1:
                 jikmoo = soup[soup.find('"sub_categories":') + 18: soup.find('],"position":') - 2]
-        
+            
             jikmoo_list = []
             current_item = ""
             for item in jikmoo:
@@ -90,12 +90,11 @@ while completed_count < max_completed_count:
             
             jikmoo_list = [item.lstrip().replace('"', '') 
                 for item in jikmoo_list]
-            # print(', '.join(jikmoo_list),end='\n\n')  
-            # 직무 리스트 전체 확인용 print
-            
-            # for item in jikmoo_list:
-            #     print(item)
-            #     직무 리스트 개별 확인용 print
+           
+            #data = {'detail': combined_text}
+            #producer = KafkaProducer(acks=0, bootstrap_servers=["43.200.4.150:9092","15.164.229.162:9092","3.39.25.90:9092"], value_serializer=lambda x: json.dumps(x, default=str, ensure_ascii=False).encode('utf-8'))
+            #producer.send('job-data', value=data)
+            time.sleep(0.1)
             job_titles = [
                     "Data Engineer",
                     "Embedded Developer",
@@ -150,13 +149,13 @@ while completed_count < max_completed_count:
                 # 채용공고 잘 가져오는 지 확인
                 with open(txt_file_path, 'w', encoding='utf-8') as txt_file:
                     for detail in details:
-                        combined_text = f"{title}\n\n직무: {jikmoo_list}\n\n{detail.get_text()}"
+                        combined_text = f"{title}\n\n직무: {jikmoo_list}\n\n{detail.get_text()}\n\n{URL.split('/')[-1]}"
                         txt_file.write(combined_text + '\n')
+                        
+                        data = {'detail': combined_text}
+                        producer = KafkaProducer(acks=0, bootstrap_servers=["43.200.4.150:9092","15.164.229.162:9092","3.39.25.90:9092"], 
+                                                 value_serializer=lambda x: json.dumps(x, default=str, ensure_ascii=False).encode('utf-8'))
+                        producer.send('job-data', value=data)
         completed_count += 1
         time.sleep(1)         
-
-
-producer.send(topic_name, value=message)
-
 driver.close()
-
