0a1179f38ec20c2b32b3fc0394101589feee08ce
sang596332 <sang596332@gmail.com>
2023-12-26 17:59:03 +0900

    crawling_pretreatment.py 파일 수정

--- crawler/crawling_pretreatment.py
+++ crawler/crawling_pretreatment.py
@@ -13,19 +13,67 @@ from selenium.common.exceptions import TimeoutException
 from kafka import KafkaProducer
 import re
 import json
+import logging
+from datetime import datetime
+
+def get_utc_now():
+    return datetime.utcnow()
+
+class JsonFormatter(logging.Formatter):
+    def __init__(self):
+        super(JsonFormatter, self).__init__()
+
+    def format(self, record):
+        # timestamp = datetime.utcfromtimestamp(record.created).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+        log_data = {
+            'timestamp': get_utc_now().strftime('%Y-%m-%d %H:%M:%S.%f'),
+            'level': record.levelname,
+            'message': f'{record.getMessage()}',
+            'name': record.name
+        }
+        record.log_data = log_data
+        return super(JsonFormatter, self).format(record)
+
+
+file_handler = logging.FileHandler('example.log', encoding='utf-8')
+formatter = JsonFormatter()
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger("crawlinglog")
+logger.setLevel(logging.INFO)
+
+logger.addHandler(file_handler)
+
+class KafkaHandler(logging.Handler):
+    def emit(self, record):
+        log_data = getattr(record, 'log_data', None)
+        if log_data:
+            producer.send('open-test', value=json.dumps(log_data, default=str, ensure_ascii=False).encode('utf-8'))
+            time.sleep(0.05)
+
+        
+producer = KafkaProducer(
+    acks=0,
+    bootstrap_servers=["54.180.149.231:9092", "54.180.96.141:9092", "54.180.81.8:9092"],
+    value_serializer=lambda x: x,
+    metadata_max_age_ms=60000,
+    request_timeout_ms=30000
+)
+
+kafka_handler = KafkaHandler()
+kafka_handler.setLevel(logging.INFO)
+kafka_handler.setFormatter(formatter)
+logger.addHandler(kafka_handler)
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
-   
 chrome_options.add_experimental_option("detach", True)
-chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
-chrome_options.add_argument('--log-level=3')
+chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+chrome_options.add_argument('--log-level=DEBUG')
 
 service = Service(executable_path=ChromeDriverManager().install())
-   
 driver = webdriver.Chrome(service=service, options=chrome_options)
-  
 driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
     "source": """
             Object.defineProperty(navigator, 'webdriver', {
@@ -33,65 +81,72 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
             })
             """
 })
-URL_LIST = []
 
-def MAKE_URL():
-    for i in range(195000, 195100, 1): # 리눅스로 크론탭할 때 변수 변경이 가능한가
-        URL = "https://www.wanted.co.kr/wd/" + str(i)
-        URL_LIST.append(URL)
+def log_crawling_start():
+    extra_data = {'timestamp': get_utc_now()}
+    logger.info('crawling start.', extra=extra_data)
 
-MAKE_URL()
+def log_crawling_success(url):
+    extra_data = {'timestamp': get_utc_now()}
+    logger.info(f'success - URL: {url}', extra=extra_data)
 
-# def MAKE_URL(start, iterations, step):
-#     URL_LIST = []
-    
-#     for _ in range(iterations):
-#         end = start + step
-#         for i in range(start, end):
-#             url = "https://www.wanted.co.kr/wd/" + str(i)
-#             URL_LIST.append(url)
-#         start = end      
-#     return URL_LIST
+def log_http_error(url, http_err):
+    extra_data = {'timestamp': get_utc_now()}
+    logger.error(f'Not found. {http_err}', extra=extra_data)
 
-# URL_LIST = MAKE_URL(195000, 100, 1)
+def log_request_error(url, req_err):
+    extra_data = {'timestamp': get_utc_now()}
+    logger.error(f'fail requests: {req_err}', extra=extra_data)
 
-# # 다음 루프를 위해 start 값을 설정
-# start_for_next_loop = URL_LIST[-1].split("/")[-1]
-# start_for_next_loop = int(start_for_next_loop) + 1
+def log_crawling_error(error):
+    extra_data = {'timestamp': get_utc_now()}
+    logger.error(f'fail crawling: {error}', extra=extra_data)
 
-# URL_LIST_NEXT_LOOP = MAKE_URL(start_for_next_loop, 100, 1)
+def log_non_dev_related(url):
+    extra_data = {'timestamp': get_utc_now()}
+    logger.info(f'this url({url})is not for developer', extra=extra_data)
+
+def make_url_list(start, end, step):
+    url_list = [f"https://www.wanted.co.kr/wd/{i}" for i in range(start, end, step)]
+    return url_list
+
+URL_LIST = make_url_list(185000, 186000, 1)
 
 completed_count = 0
 max_completed_count = len(URL_LIST)
 
-
 while completed_count < max_completed_count:
     for i, URL in enumerate(URL_LIST):
-        response = requests.get(URL)
-        if response.status_code == 200:
-            
+        try:
+            response = requests.get(URL)
+            response.raise_for_status()
+
             driver.get(URL)
-            try:
-                WebDriverWait(driver, 20).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
-            except TimeoutException:
-                print("페이지 로딩 시간 초과")
+            WebDriverWait(driver, 20).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
+
+            log_crawling_start()
 
             page_source = driver.page_source
             soup = bs(page_source, 'html.parser')
-            
-            companyname = soup.select("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > \
-                div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > section.JobHeader_className__HttDA > \
-                div:nth-child(2) > span.JobHeader_companyNameText__uuJyu > a")
+
+            companyname = soup.select("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > "
+                                      "div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > "
+                                      "section.JobHeader_className__HttDA > div:nth-child(2) > "
+                                      "span.JobHeader_companyNameText__uuJyu > a")
 
             if companyname:
-                data_company_name = companyname[0]['data-company-name']            
-            
+                data_company_name = companyname[0]['data-company-name']
+
             title = soup.title.text
-            base_selector = "#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > \
-                  div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > \
-                  div.JobContent_descriptionWrapper__SM4UD > \
-                  section.JobDescription_JobDescription__VWfcb > p:nth-child({}) > {}"
-            
+            base_selector = "#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > " \
+                             "div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > " \
+                             "div.JobContent_descriptionWrapper__SM4UD > " \
+                             "section.JobDescription_JobDescription__VWfcb > p:nth-child({}) > {}"
+            workplace = soup.select_one('#__next > div.JobDetail_cn__WezJh > '
+                                         'div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > '
+                                         'div.JobContent_className___ca57 > section.JobHeader_className__HttDA > '
+                                         'div:nth-child(2) > span.JobHeader_pcLocationContainer__xRwIv')
+
             companydescription = str(soup.select(base_selector.format(1, 'span')))
             mainbusiness = str(soup.select(base_selector.format(3, 'span')))
             qualifications = str(soup.select(base_selector.format(5, 'span')))
@@ -100,80 +155,79 @@ while completed_count < max_completed_count:
             technologystack = str(soup.select(base_selector.format(11, 'div')))
 
             soup = str(soup)
-
-            if soup.find('"occupationalCategory":')!= -1:
-                if soup.find('"validThrough":')!= -1:
+            if '"occupationalCategory":' in soup:
+                if '"validThrough":' in soup:
                     jikmoo = soup[soup.find('"occupationalCategory":') + 24: soup.find('"validThrough":') - 2]
                 else:
                     jikmoo = soup[soup.find('"occupationalCategory":') + 24: soup.find('"employmentType"') - 2]
-            elif soup.find('"sub_categories":')!= -1:
-                jikmoo = soup[soup.find('"sub_categories":') + 18: soup.find('],"position":') - 2]
-            
-            jikmoo_list = []
-            current_item = ""
-            for item in jikmoo:
-                if item == ",":
-                    jikmoo_list.append(current_item)
-                    current_item = ""
-                else:
-                    current_item += item
-            jikmoo_list.append(current_item)
-            
-            jikmoo_list = [item.lstrip().replace('"', '') 
-                for item in jikmoo_list]   
+            elif '"sub_categories":' in soup:
+                jikmoo = soup[soup.find('"sub_categories":') + 18: soup.find('],"position":') - 1]
+
+            jikmoo_list = [item.lstrip().replace('"', '') for item in jikmoo.split(",")]
+
             time.sleep(1)
-            
+
             job_titles = [
-                    "Data Engineer","Embedded Developer","Java Developer",".NET Developer",
-                    "Network Administrator","System, Network Administrator","Front-end Engineer","Security Engineer",
-                    "Hardware Engineer","DevOps""System Admin""DevOps / System Admin",
-                    "Test Engineer","QA, Test Engineer","Android Developer","iOS Developer",
-                    "Chief Information Officer","CIO, Chief Information Officer","Chief Technology Officer","CTO, Chief Technology Officer",
-                    "Back-end Engineer","Web Developer","Product Manager","Development Manager",
-                    "PHP Developer","Ruby on Rails Developer","Node.js Developer","Voice Engineer",
-                    "Video, Voice Engineer","Graphics Engineer","Python Developer","C++ Developer",
-                    "C, C++ Developer","Web Publisher","BI Engineer","Data Scientist","Big Data Engineer",
-                    "Technical Support","Blockchain Platform Engineer","Machine Learning Engineer","Software Engineer",
-                    "Cross-platform App Developer","VR Engineer","ERP Consultant","Database Administrator"
-                ]
+                "Data Engineer", "Embedded Developer", "Java Developer", ".NET Developer",
+                "Network Administrator", "System, Network Administrator", "Front-end Engineer", "Security Engineer",
+                "Hardware Engineer", "DevOps", "System Admin", "DevOps / System Admin",
+                "Test Engineer", "QA, Test Engineer", "Android Developer", "iOS Developer",
+                "Chief Information Officer", "CIO, Chief Information Officer", "Chief Technology Officer",
+                "CTO, Chief Technology Officer",
+                "Back-end Engineer", "Web Developer", "Product Manager", "Development Manager",
+                "PHP Developer", "Ruby on Rails Developer", "Node.js Developer", "Voice Engineer",
+                "Video, Voice Engineer", "Graphics Engineer", "Python Developer", "C++ Developer",
+                "C, C++ Developer", "Web Publisher", "BI Engineer", "Data Scientist", "Big Data Engineer",
+                "Technical Support", "Blockchain Platform Engineer", "Machine Learning Engineer", "Software Engineer",
+                "Cross-platform App Developer", "VR Engineer", "ERP Consultant", "Database Administrator"
+            ]
 
             if any(job in job_titles for job in jikmoo_list):
                 cleaned_title = re.sub(r'[\\/*?:"<>]', '', re.sub(r'\| 원티드', '', str(data_company_name)))
-                txt_file_path = f"{cleaned_title} _{URL.split('/')[-1]}.txt"
+                txt_file_path = f"test_{cleaned_title}_{URL.split('/')[-1]}.txt"
                 print(f"채용 공고 : {cleaned_title}/{URL.split('/')[-1]}")
 
                 with open(txt_file_path, 'w', encoding='utf-8') as txt_file:
-                    combined_text = f"{title}\n\n직무: {jikmoo_list}\n\n"\
-                                    f"{companydescription}\n\n"\
-                                    "주요업무\n"\
-                                    f"{mainbusiness}\n\n"\
-                                    "자격요건\n"\
-                                    f"{qualifications}\n\n"\
-                                    "우대사항\n"\
-                                    f"{preferential}\n\n"\
-                                    "혜택 및 복지\n"\
-                                    f"{welfare}\n\n"\
-                                    "기술스택\n"\
-                                    f"{technologystack}\n\n"\
-                                    f"https://www.wanted.co.kr/wd/{URL.split('/')[-1]}\n"\
-                                    # "마감일\n"\
-                                    # f"{deadline}\n\n"\
-                                    # "근무지역\n"\
-                                    # f"{workplace}\n\n"\
-                                    
-                    combined_text_cleaned = combined_text.replace('<div class="SkillItem_SkillItem__E2WtM">', ', ')
-                    combined_text_cleaned = re.sub(r'<span.*?>|amp;|<\/span>|<div.*?>|<\/div>|<h3>|<\/h3>|\]|\[|,', '', combined_text_cleaned)
+                    combined_text = f"{title}\n" \
+                                    f"{cleaned_title}\n" \
+                                    "직무 : " f"{jikmoo_list}\n" \
+                                    "근무지역 : " f"{workplace}\n" \
+                                    f"{companydescription}\n" \
+                                    "주요업무\n" \
+                                    f"{mainbusiness}\n" \
+                                    "자격요건\n" \
+                                    f"{qualifications}\n" \
+                                    "우대사항\n" \
+                                    f"{preferential}\n" \
+                                    "혜택 및 복지\n" \
+                                    f"{welfare}\n" \
+                                    "기술스택\n" \
+                                    f"{technologystack}\n" \
+                                    f"https://www.wanted.co.kr/wd/{URL.split('/')[-1]}\n"
+
+                    combined_text_cleaned = re.sub(r'<span.*?>|amp;|<\/span>|<div.*?>|<\/div>|<h3>|<\/h3>|\]|\[|,',
+                                                  '', combined_text)
                     combined_text_cleaned = re.sub(r'<br/>', '\n', combined_text_cleaned)
                     txt_file.write(combined_text_cleaned + '\n')
-                    data = {combined_text_cleaned.replace('\n', ' ')}
+                    data = {'message': combined_text_cleaned.replace('\n', ' ')}
                     serialized_data = json.dumps(data, default=str, ensure_ascii=False).encode('utf-8')
 
-                    # producer = KafkaProducer(
-                    #         acks=0,
-                    #         bootstrap_servers=["13.125.213.220:9092", "54.180.81.131:9092", "54.180.91.234:9092"],
-                    #         value_serializer=lambda x: x,)
-                    # producer.send('job-data', value=serialized_data)
-                    # time.sleep(0.05)
+                    producer.send('job-data', value=serialized_data)
+                        
+                    log_crawling_success(URL)
+            else:
+                log_non_dev_related(URL)
+
+        except requests.exceptions.HTTPError as http_err:
+            log_http_error(URL, http_err)
+        except requests.exceptions.RequestException as req_err:
+            log_request_error(URL, req_err)
+        except Exception as e:
+            log_crawling_error(e)
+            continue
+
         completed_count += 1
-        time.sleep(1)         
+        time.sleep(1)
+
+logger.removeHandler(file_handler)
 driver.close()
