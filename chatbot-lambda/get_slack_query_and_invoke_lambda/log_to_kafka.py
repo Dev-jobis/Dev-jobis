60aada75139c406efa08bc948d8d7181891baf20
norahbox <parksso0530@gmail.com>
2023-12-28 19:25:41 +0900

    kafka에 로그를 보내기 위한 logging 코드 추가

---
+++ chatbot-lambda/get_slack_query_and_invoke_lambda/log_to_kafka.py
@@ -0,0 +1,32 @@
+import json
+from datetime import datetime
+import logging
+from kafka import KafkaProducer
+import time
+
+kafka_log_producer = KafkaProducer(
+    bootstrap_servers=[
+        "175.0.0.139:9092",  # TODO: parameter store로 대체
+        "175.0.0.155:9092",
+        "175.0.0.170:9092",
+    ],
+    value_serializer=str.encode,
+)
+
+
+class KafkaHandler(logging.Handler):
+    def emit(self, record):
+        log_data = self.format(record)
+        kafka_log_producer.send("open-test", value=log_data)
+        time.sleep(0.05)
+
+
+class JsonFormatter(logging.Formatter):  # TODO: 재사용성
+    def format(self, record):
+        log_data = {
+            "timestamp": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "name": record.name,
+        }
+        return json.dumps(log_data)
