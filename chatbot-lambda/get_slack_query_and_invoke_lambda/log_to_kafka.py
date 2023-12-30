0b7361dbc6b36b46df8ce6f498ce856d1c09c53a
norahbox <parksso0530@gmail.com>
2023-12-30 11:56:57 +0900

    로그 공통 모듈 작성
    gist에도 업로드 했습니다. https://gist.github.com/norahBOX/38706fad90f1506d13f4473345a25fdb

--- chatbot-lambda/get_slack_query_and_invoke_lambda/log_to_kafka.py
+++ chatbot-lambda/get_slack_query_and_invoke_lambda/log_to_kafka.py
@@ -1,8 +1,8 @@
 import json
 from datetime import datetime
 import logging
-from kafka import KafkaProducer
 import time
+from kafka import KafkaProducer
 
 kafka_log_producer = KafkaProducer(
     bootstrap_servers=[
@@ -17,16 +17,38 @@ kafka_log_producer = KafkaProducer(
 class KafkaHandler(logging.Handler):
     def emit(self, record):
         log_data = self.format(record)
-        kafka_log_producer.send("open-test", value=log_data)  # TODO: 재사용성
-        time.sleep(0.05)
+        kafka_log_producer.send("open-test", value=log_data)
+        time.sleep(0.1)
 
 
-class JsonFormatter(logging.Formatter):  # TODO: 재사용성
-    def format(self, record):
+class CustomLogger:
+    def __init__(self, service_name, default_level=logging.DEBUG):
+        self.service_name = service_name
+        self.default_level = default_level  # 기본으로 DEBUG로 세팅
+        self.logger = self.define_logger()
+
+    def define_logger(self):
+        logger = logging.getLogger(self.service_name)
+        logger.setLevel(self.default_level)
+        logger.addHandler(KafkaHandler())
+        return logger
+
+    def send_json_log(
+        self,
+        message,
+        log_level=None,
+        timestamp=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"),
+        extra_data="",
+    ):
+        if log_level is None:
+            log_level = self.default_level
+
         log_data = {
-            "timestamp": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"),
-            "level": record.levelname,
-            "message": record.getMessage(),
-            "name": record.name,
+            "timestamp": timestamp,
+            "level": logging.getLevelName(log_level),
+            "message": message,
+            "service_name": self.service_name,
+            "extra_data": extra_data,
         }
-        return json.dumps(log_data)
+        json_log_data = json.dumps(log_data)
+        self.logger.log(log_level, json_log_data)
