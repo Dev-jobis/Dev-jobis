b6240486697298c311688a362a5125b78ca397e7
norahbox <parksso0530@gmail.com>
2023-12-30 12:24:13 +0900

    사용하지 않는 주석 삭제

--- chatbot-lambda/reply_rag/utils.py
+++ chatbot-lambda/reply_rag/utils.py
@@ -2,9 +2,9 @@ import boto3
 
 ssm = boto3.client("ssm", region_name="ap-northeast-2")
 parameter_name = "/SESAC/SLACK/SIGNING_SECRET"
-response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)  # 복호화 옵션
+response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
 SLACK_SIGNING_SECRET = response["Parameter"]["Value"]
 
 parameter_name = "/SESAC/SLACK/BOT_TOKEN"
-response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)  # 복호화 옵션
+response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
 SLACK_BOT_TOKEN = response["Parameter"]["Value"]
