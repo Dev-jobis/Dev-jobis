2ec2970f7c280a7a3fcff8688280075a76de1de1
ageofaz <152600524+ageofaz@users.noreply.github.com>
2024-01-01 19:43:45 +0900

    kafka-cluster, bastion-hosts 파일에 스크립트 추가 terraform-install.MD 추가

--- terraform/bastion-host.tf
+++ terraform/bastion-host.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "bastion-host" {
   vpc_security_group_ids = [aws_security_group.bastion.id]
   subnet_id = aws_subnet.public_subnet1.id
   associate_public_ip_address = true
+#known_hosts 삭제
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo rm -rf /home/ec2-user/.ssh/known_hosts
+  EOF
+
   tags = {
    Name = "bastion-host"
 }
--- terraform/kafka-cluster.tf
+++ terraform/kafka-cluster.tf
@@ -8,6 +8,13 @@ resource "aws_instance" "kafka-1" {
   subnet_id = aws_subnet.private_subnet1.id
   private_ip = "175.0.0.139"
   iam_instance_profile = data.aws_iam_instance_profile.existing_role.role_name # Fluentd S3 접근 허용
+# 카프카 재시작 및 fluentd 시작
+  user_data = <<-EOF
+        #!/bin/bash
+        sleep 15
+        sudo systemctl restart kafka.service
+        sudo systemctl enable --now fluentd.service
+        EOF
 
   tags = {
     Name = "kafka-1"
@@ -23,6 +30,13 @@ resource "aws_instance" "kafka-2" {
   associate_public_ip_address = false
   subnet_id = aws_subnet.private_subnet2.id
   private_ip = "175.0.0.155"
+# 카프카 재시작
+  user_data = <<-EOF
+        #!/bin/bash
+        sleep 15
+        sudo systemctl restart kafka.service
+        EOF
+
   tags = {
     Name = "kafka-2"
   }
@@ -36,6 +50,13 @@ resource "aws_instance" "kafka-3" {
   associate_public_ip_address = false
   subnet_id = aws_subnet.private_subnet3.id
   private_ip = "175.0.0.170"
+# 카프카 재시작
+  user_data = <<-EOF
+        #!/bin/bash
+        sleep 16
+        sudo systemctl restart kafka.service
+        EOF
+
   tags = {
     Name = "kafka-3"
   }
