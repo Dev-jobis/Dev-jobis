0ccfe050cc72c004309a679a8c34641d0d432c22
sang596332 <sang596332@gmail.com>
2024-01-02 20:31:33 +0900

    parameter store에서 url 시작 number가지고 오기(현재 설정 100000)

--- crawler/utils.py
+++ crawler/utils.py
@@ -2,7 +2,5 @@ import boto3
 
 ssm = boto3.client("ssm", region_name="ap-northeast-2")
 parameter_name = "/SESAC/URL/RANGE"
-response = ssm.get_parameter(Name=parameter_name, WithDecryption=False)  # 또는 생략 가능
+response = ssm.get_parameter(Name=parameter_name, WithDecryption=False)
 URL_RANGE_value = response["Parameter"]["Value"]
-
-print(URL_RANGE_value)
