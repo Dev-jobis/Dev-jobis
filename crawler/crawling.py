89b35a551dd6238bec785dfd53c2e9d8df95479e
sang596332 <sang596332@gmail.com>
2024-01-03 11:16:31 +0900

    "직무" 분야 찾는 코드 하나의 조건문으로 수정

--- crawler/crawling.py
+++ crawler/crawling.py
@@ -77,19 +77,24 @@ for url in url_list:
             log_level=logging.INFO,
         )
         # 2. 개발 공고인지 확인
-        if '"occupationalCategory":' in page_source:
-            if '"validThrough":' in page_source:
-                jikmoo = page_source[
-                    page_source.find('"occupationalCategory":')
-                    + 24 : page_source.find('"validThrough":')
-                    - 2
-                ]
-            else:
-                jikmoo = page_source[
-                    page_source.find('"occupationalCategory":')
-                    + 24 : page_source.find('"employmentType"')
-                    - 2
-                ]
+        if (
+            '"occupationalCategory":' in page_source
+            and '"validThrough":' in page_source
+        ):
+            jikmoo = page_source[
+                page_source.find('"occupationalCategory":')
+                + 24 : page_source.find('"validThrough":')
+                - 2
+            ]
+        elif (
+            '"occupationalCategory":' in page_source
+            and '"employmentType"' in page_source
+        ):
+            jikmoo = page_source[
+                page_source.find('"occupationalCategory":')
+                + 24 : page_source.find('"employmentType"')
+                - 2
+            ]
         elif '"sub_categories":' in page_source:
             jikmoo = page_source[
                 page_source.find('"sub_categories":')
