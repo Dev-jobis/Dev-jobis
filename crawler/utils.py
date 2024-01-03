6785f0aa1cfef0e753fcbc972e5f8bd92e0db0f4
sang596332 <sang596332@gmail.com>
2024-01-03 14:40:53 +0900

    utils.py에 parameter 값 수정하는 코드 추가
    기존값 : url_range_value에서 코드가 한번 실행되고 나면 new_url_range_value를 통해 200이 추가된 값으로 수정됨.
    이후 크롤링은 new_url_range_value 값으로 바뀌어 진행

--- crawler/utils.py
+++ crawler/utils.py
@@ -2,5 +2,15 @@ import boto3
 
 ssm = boto3.client("ssm", region_name="ap-northeast-2")
 parameter_name = "/SESAC/URL/RANGE"
+
 response = ssm.get_parameter(Name=parameter_name, WithDecryption=False)
-URL_RANGE_value = response["Parameter"]["Value"]
+url_range_value = response["Parameter"]["Value"]
+
+new_url_range_value = str(int(url_range_value) + 200)
+
+response_put = ssm.put_parameter(
+    Name=parameter_name,
+    Value=new_url_range_value,
+    Type="String",
+    Overwrite=True,
+)
