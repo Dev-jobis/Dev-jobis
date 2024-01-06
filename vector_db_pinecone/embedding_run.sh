11a5e78bab12395b58660252c1399a4026b78579
norahbox <parksso0530@gmail.com>
2024-01-06 22:32:52 +0900

    shell script 생성 및 README 수정

---
+++ vector_db_pinecone/embedding_run.sh
@@ -0,0 +1,7 @@
+REPOPATH=/home/ec2-user/Dev-jobis/vector_db_pinecone
+cd $REPOPATH
+echo $(pwd)
+
+echo $(date) ... Start Embedding
+sudo docker run -it --rm -e AWS_ACCESS_KEY_ID=`cat .accesskeyid` -e AWS_SECRET_ACCESS_KEY=`cat .accesskey` embedding > test.log
+echo $(date) ... Finish.
